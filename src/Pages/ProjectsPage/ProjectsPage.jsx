0c08cad2aabc525d326c9cc01e3c506ed64b7f77
M-Ahmad676 <125358039+M-Ahmad676@users.noreply.github.com>
2025-07-13 23:58:00 +0500

    update 2.1: project section (progress 10%)

--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import GradientText from '../../components/GradientText/GradientText'
+import { IoGlobeOutline } from "react-icons/io5";
 
 export default function ProjectsPage() {
+
+  const projects = [
+
+    {
+      image_path:"/iris_engine.webp",
+      title:"Iris Engine",
+      type:"Vide Game Store",
+      description:"An Video Game store featuring latest Pc game, developed using React Js, Vite, Tailwind CSS",
+      url:"https://iris-engine.vercel.app/"
+    }
+
+  ]
+
   return (
-    <div className='max-w-screen-2xl mx-auto my-20'>
+    <div className='max-w-screen-2xl mx-auto my-20 px-20'>
 
       <div className='text-[3rem] font-bold flex justify-center'>
       <GradientText 
@@ -14,8 +28,26 @@ export default function ProjectsPage() {
       </GradientText>
       </div>
 
+      <div className='grid grid-cols-3 auto-cols-max'>
+     
+     {projects.map((project,index) => (
+     
+     <div key={index} className='shadow-lg shadow-purple-600/30'> 
+      <div>
+       <img src={project.image_path} alt="thumbnail" className='rounded-t-xl'/>
+      </div>
+      <div className='p-7 space-y-3'>
+       <h2 className='text-white text-[1.7rem] font-bold'>{project.title}</h2>
+       <p className='text-gray-500 '>{project.description}</p>
+       <a href={project.url} className='flex items-center gap-x-2 text-purple-500 text-[1.1rem] font-medium'><IoGlobeOutline className='text-[1.5rem]'/> Live</a>
+      </div>
 
-      
+     </div>
+  
+     ))}
+      </div>
+
+
     </div>
   )
 }
