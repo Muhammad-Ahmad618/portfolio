b9c03193596410200eb5517397e8e0317b171acf
M-Ahmad676 <125358039+M-Ahmad676@users.noreply.github.com>
2025-08-17 10:59:54 +0500

    version 2.7.1: Contact form ( made functional )

--- src/components/ReactForms/React_contact_form.jsx
+++ src/components/ReactForms/React_contact_form.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { FaTelegramPlane } from "react-icons/fa";
 
 export default function React_contact_form() {
+
+     const [status, setStatus] = useState('')
 
     const {
         register,
@@ -12,22 +15,45 @@ export default function React_contact_form() {
         formState: {errors}
     } = useForm()
 
-    const onsubmit = (data) => console.log(data)
+     const onSubmit = async(data) => {
+      
+      try{
+        const response = await axios.post('https://formspree.io/f/mgvzbaww', data)
+        
+        if(response.status == 200){
+          console.log(response.status)
+          setStatus("Thank You for your Message! I'll get back to you shortly")
+          reset()
+        }
+        else{
+          console.log(response.status)
+          setStatus("Failed to Send Message")
+        }
+      }
+      catch(error){
+        setStatus("Error while sending Message! Please Try Again") 
+      }
+
+    }
 
     console.log(watch("example"))
 
   return (
     <div className='border-2 border-purple-400 bg-gradient-to-r from-gray-950 to-gray-900 shadow-md shadow-purple-500/30 p-10 rounded-2xl'>
 
-        <form onSubmit={handleSubmit(onsubmit)} className='flex flex-col gap-y-10'>
+        <form 
+       onSubmit={handleSubmit(onSubmit)}
+       className='flex flex-col gap-y-10'>
            
            <input type='text' {...register("Name", {required:"Name is Required"})}
            placeholder='Name'
            className='border-b-1 border-purple-600 text-white focus:outline-0 py-2'
+           name='Name'
            />
            <input type='email' {...register("Email", {required:"Email is Required"})}
            placeholder='Email'
             className='border-b-1 border-purple-600 text-white focus:outline-0 py-2'
+            name='Email'
            />
            <textarea name="message" {...register("Message", {required:"Message Box is Empty"})}
            cols="40"
@@ -39,8 +65,9 @@ export default function React_contact_form() {
           <button type="submit" value="Send Message" className='text-white py-2 rounded-lg cursor-pointer bg-gradient-to-r from-purple-700 to-blue-600 transition-all duration-300 ease-in-out hover:opacity-80 flex justify-center gap-x-3 font-semibold'>Send Message
             <FaTelegramPlane className='text-[1.3rem]'/>
           </button>
-           
-
+           {status &&
+           <p className={`${response.status == 200 ? 'text-purple-500' : 'text-red-600'} text-sm`}>{status}</p>
+           }
         </form>
     
     </div>
